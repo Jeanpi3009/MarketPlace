89de5ad3d8677708dd3f22b8b89cc034452f3dcf
Jean <jeanpierreloaizaleon@gmail.com>
2022-12-09 23:04:34 -0500

    Se crea carrito

---
+++ products.js
@@ -0,0 +1,12 @@
+const products = [
+    {id: 1, name: 'PS4', price: 1000000000,img: 'source/producto1.jpg'},
+    {id: 2, name: 'Mando-PS4', price: 500000,img: 'source/producto2.jpg'},
+    {id: 3, name: 'Audifonos', price: 30000,img: 'source/producto3.jpg'},
+    {id: 4, name: 'Watch Dogs', price: 150000,img: 'source/producto4.jpg'},
+    {id: 5, name: 'God of War', price: 200000,img: 'source/producto5.jpg'},
+    {id: 6, name: 'Call of Duty', price: 100000,img: 'source/producto6.jpg'},
+    {id: 7, name: 'Portador', price: 70000,img: 'source/producto7.jpg'},
+    {id: 8, name: 'PS5', price: 2000000000,img: 'source/producto8.jpg'},
+    {id: 9, name: 'Mando-PS5', price: 1000000,img: 'source/producto9.jpg'},
+    {id: 10, name: 'Forro-mando', price: 25000,img: 'source/producto10.jpg'},
+  ];
